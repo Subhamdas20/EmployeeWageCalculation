1f265ef64acc183348d3992fa7704c5be9827ed5
Subhamdas20 <subham21.sd@gmail.com>
2021-09-16 01:08:11 +0530

    checking attendence status

--- src/com/bz/wagecalculationusingoopsconcept/DisplayWelcome.java
+++
@@ -1,8 +0,0 @@
-package com.bz.wagecalculationusingoopsconcept;
-
-public class DisplayWelcome {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation program");
-
-    }
-}
