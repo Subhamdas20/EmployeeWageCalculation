2420d3fbe1b70378c8eea18e77a2f499184eb676
Subhamdas20 <subham21.sd@gmail.com>
2021-09-16 01:04:24 +0530

    Display welcome messege

---
+++ src/com/bz/wagecalculationusingoopsconcept/DisplayWelcome.java
@@ -0,0 +1,8 @@
+package com.bz.wagecalculationusingoopsconcept;
+
+public class DisplayWelcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation program");
+
+    }
+}
