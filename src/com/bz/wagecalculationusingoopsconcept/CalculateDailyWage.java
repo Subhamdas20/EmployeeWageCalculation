464e002148a520efa12384c6365746644aea8822
Subhamdas20 <subham21.sd@gmail.com>
2021-09-16 01:14:09 +0530

    Calculated Daily Wage

---
+++ src/com/bz/wagecalculationusingoopsconcept/CalculateDailyWage.java
@@ -0,0 +1,10 @@
+package com.bz.wagecalculationusingoopsconcept;
+
+public class CalculateDailyWage {
+    public static void main(String[] args) {
+            int wagePerHour = 20;
+            int hoursPerDay = 8;
+            System.out.println("The Daily Wage of Employee = " + wagePerHour*hoursPerDay + " RS");
+
+        }
+}
