923991066c14104fb47b5cfd0a9d7dce76bbac4d
Subhamdas20 <subham21.sd@gmail.com>
2021-09-20 23:32:05 +0530

    Calculated total wage for multiple employees

--- src/com/bz/wagecalculationusingoopsconcept/WageCalculations.java
+++ src/com/bz/wagecalculationusingoopsconcept/WageCalculations.java
@@ -6,19 +6,19 @@ public class WageCalculations {
     ArrayList<WageCalculations> wage = new ArrayList<>();
     static int hoursPerDay = 8;
     static int monthlyWage;
-//    static int totalWage;
+    //    static int totalWage;
     public static int CalculateMonthlyWageBasedOnTotalDaysOrTotalHours(int userInput , int workingDays , int hoursPerMonth, int wagePerHour){
         if (userInput == 1) {
-             monthlyWage=wagePerHour * hoursPerDay * workingDays;
+            monthlyWage=wagePerHour * hoursPerDay * workingDays;
             System.out.println("The Monthly Wage of Employee based on total working days=" + monthlyWage + " Rs");
         }
         else if (userInput == 2) {
-             monthlyWage=wagePerHour * hoursPerMonth;
+            monthlyWage=wagePerHour * hoursPerMonth;
             System.out.println("The Monthly Wage of Employee based on total working hours=" + monthlyWage + " Rs");
         }
         else{
             System.out.println("Invalid Input from User");}
-    return monthlyWage;
+        return monthlyWage;
     }
     public static   void wageBuilder(){
         int totalWage = 0;
@@ -41,6 +41,6 @@ public class WageCalculations {
         System.out.println("Total wage is "+totalWage);
     }
     public static void main(String[] args) {
-       wageBuilder();
+        wageBuilder();
     }
 }
